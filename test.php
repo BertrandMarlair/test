e2f6890894f2842a752ff3d072855cc58427e7ba
bertrandBecode <bertrand@becode.org>
2017-11-22 11:34:41 +0100

    test

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+try
+{
+	// On se connecte à MySQL
+	$bdd = new PDO('mysql:host=ec2-54-75-233-162.eu-west-1.compute.amazonaws.com;dbname=ddl32bedp88msc;charset=utf8', 'topizpowfzcgrh', '35dafbbd85345da46254cb6fd4d53d9fd94f73e06851bdf4b98bec08a649ede0');
+}
+catch(Exception $e)
+{
+	// En cas d'erreur, on affiche un message et on arrête tout
+        die('Erreur : '.$e->getMessage());
+}
