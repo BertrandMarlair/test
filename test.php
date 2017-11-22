027606dc833040ad6d3c7db3459223da2fc116e6
bertrandBecode <bertrand@becode.org>
2017-11-22 11:35:47 +0100

    test

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 try
 {
 	// On se connecte à MySQL
-	$bdd = new PDO('mysql:host=ec2-54-75-233-162.eu-west-1.compute.amazonaws.com;dbname=ddl32bedp88msc;charset=utf8', 'topizpowfzcgrh', '35dafbbd85345da46254cb6fd4d53d9fd94f73e06851bdf4b98bec08a649ede0');
+	$bdd = new PDO('mysql:host=ec2-54-75-233-162.eu-west-1.compute.amazonaws.com;dbname=ddl32bedp88msc;', 'topizpowfzcgrh', '35dafbbd85345da46254cb6fd4d53d9fd94f73e06851bdf4b98bec08a649ede0');
 }
 catch(Exception $e)
 {
